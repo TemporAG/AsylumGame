7703b41877e88c5d64335fc17dfac8c61dfd011d
TemporAG <164005931+TemporAG@users.noreply.github.com>
2024-03-20 10:38:58 +0000

    added a throw and pick-up mechanic

--- Asylumgame/Assets/Scripts/ItemPickupThrow.cs
+++ Asylumgame/Assets/Scripts/ItemPickupThrow.cs
@@ -7,9 +7,16 @@ public class ItemPickupThrow : MonoBehaviour
 {
     public Camera fpsCam;
     //public ItemScript ItemPick;
+    public GameObject player;
+    public Transform holdPos;
 
-    public int range = 100;
+    public float range = 1.9f;
+    public float throwForce = 500f;
+    public float pickUpRange = 1.9f;
 
+    private GameObject heldObj;
+    private Rigidbody heldObjRb;
+    private bool canDrop = true;
     public float PickupRate = 10.0f;
     private float nextTimeToPickup = 0f;
 
@@ -22,10 +29,8 @@ public class ItemPickupThrow : MonoBehaviour
     //private AIscript aIscript;
 
 
-    private void Start()
-    {
 
-    }
+
     void Update()
     {
         origin = transform.position;
@@ -33,8 +38,91 @@ public class ItemPickupThrow : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && Time.time >= nextTimeToPickup)
         {
             nextTimeToPickup = Time.time + 2f / PickupRate;
-            Shoot(); 
+            Shoot();
         }
+        origin = transform.position;
+        direction = fpsCam.ScreenPointToRay(Input.mousePosition).direction;
+        if (Input.GetKeyDown(KeyCode.E) && Time.time >= nextTimeToPickup)
+        {
+            nextTimeToPickup = Time.time + 2f / PickupRate;
+            //Shoot(); 
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObj == null)
+            {
+
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+                {
+
+                    if (hit.transform.gameObject.tag == "Holdable")
+                    {
+
+                        PickUpObject(hit.transform.gameObject);
+                    }
+                }
+            }
+            else
+            {
+                if (canDrop == true)
+                {
+
+                    DropObject();
+                }
+            }
+        }
+        if (heldObj != null)
+        {
+            MoveObject();
+
+            if (Input.GetKeyDown(KeyCode.Mouse1) && canDrop == true)
+            {
+
+                ThrowObject();
+            }
+
+        }
+    }
+
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>())
+        {
+            heldObj = pickUpObj;
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>();
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform;
+
+            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+        }
+    }
+    void DropObject()
+    {
+
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+    void MoveObject()
+    {
+
+        heldObj.transform.position = holdPos.transform.position;
+    }
+
+    void ThrowObject()
+    {
+
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObjRb.AddForce(transform.forward * throwForce);
+        heldObj = null;
+
     }
 
     void Shoot()
@@ -44,19 +132,9 @@ public class ItemPickupThrow : MonoBehaviour
         {
             if (hit.transform.CompareTag("Consumable"))
             {
+                Debug.Log("e");
                 //do something
-            }
-            else if (hit.transform.CompareTag("Holdable"))
-            {
-                //full pickup
-
-                //ItemPick = hit.collider.GetComponent;
-                //hit.Bottle.transform.parent = .Hand...;
-                // 
             }
         }
     }
-
-    
 }
-
