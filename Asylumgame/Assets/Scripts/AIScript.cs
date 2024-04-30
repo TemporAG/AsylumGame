2d84ab0652e6bb15ff67dcdce9757e221d50d4d0
TemporAG <164005931+TemporAG@users.noreply.github.com>
2024-04-30 09:51:11 +0100

    patrol

--- Asylumgame/Assets/Scripts/AIScript.cs
+++ Asylumgame/Assets/Scripts/AIScript.cs
@@ -8,6 +8,10 @@ public class AiScript : MonoBehaviour
     public Transform player;
     public static bool SphereCast;
 
+    public float range; 
+
+    public Transform centrePoint;
+
     public LayerMask layerMask;
 
     public float radius;
@@ -103,6 +107,33 @@ public class AiScript : MonoBehaviour
         {
             Red.SetActive(false);
             White.SetActive(true);
+        }
+
+        if (agent.remainingDistance <= agent.stoppingDistance) //done with path
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
+                agent.SetDestination(point);
+            }
+        }
+
+        bool RandomPoint(Vector3 center, float range, out Vector3 result)
+        {
+
+            Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+            {
+                //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+                //or add a for loop like in the documentation
+                result = hit.position;
+                return true;
+            }
+
+            result = Vector3.zero;
+            return false;
         }
     }
 
